297b5b686e5360036f8ecdfa35ed9bf77a161341
Miguel Olave <molavec@gmail.com>
2023-03-25 00:33:50 -0300

    add menu and logo

--- wp-content/themes/cookies-cake/index.php
+++ wp-content/themes/cookies-cake/index.php
@@ -13,6 +13,23 @@ $PAGE_TITLE = 'COOKIE CAKE'
 <body>
   <!-- HEADER -->
   <header class='header'>
+    <div class="container bg-red-600">
+      <div class="logo-header">
+        <img src="<?php echo get_template_directory_uri()?>/img/logo.png" alt="" width="200">
+        <p>header</p>
+      </div>
+      
+      <div class="menu">
+        <?php 
+          $args = array(
+            'theme_location' => 'menu-principal',
+          );
+
+          wp_nav_menu($args);
+        ?>
+      </div>
+
+    </div>
     
   </header>
 
